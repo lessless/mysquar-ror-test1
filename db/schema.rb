4c4194e078c068e045e04029afabda49934739dc
lessless <me@lessless.pp.ua>
2015-09-16 17:52:46 +0700

    added like model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831075916) do
+ActiveRecord::Schema.define(version: 20150916104442) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "text"
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Like', type: :model do
+RSpec.describe Like, type: :model do
   it { should validate_presence_of(:post_id) }
   it { should validate_presence_of(:user_id) }
   it { should belong_to(:post) }
